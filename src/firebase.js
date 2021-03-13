5366ce994a10be941d9ef0c9ef5cb4c31de1d6aa
Rodrigo Ortiz de Zarate <rodrigoodz_6@hotmail.com>
2021-03-12 22:39:45 -0300

    configurado firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkZc82_zUTX4oQaHsRHKw7GAMBN67ag68",
+  authDomain: "twitter-clone-395c5.firebaseapp.com",
+  projectId: "twitter-clone-395c5",
+  storageBucket: "twitter-clone-395c5.appspot.com",
+  messagingSenderId: "722935752773",
+  appId: "1:722935752773:web:5caeac8990a4d50c949b2c",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
